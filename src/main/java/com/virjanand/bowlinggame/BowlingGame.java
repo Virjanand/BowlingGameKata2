5362f959348c2e6963a2e2e75f91047c01a86e33
Virjanand <virjanand@virjanand.com>
2020-07-19 09:46:46 +0200

    Red test: sum rolls: 20 x 1 -> score = 20

--- src/main/java/com/virjanand/bowlinggame/BowlingGame.java
+++ src/main/java/com/virjanand/bowlinggame/BowlingGame.java
@@ -1,6 +1,12 @@
 package com.virjanand.bowlinggame;
 
+import java.util.List;
+
 public class BowlingGame {
+    public BowlingGame(List<String> asList) {
+
+    }
+
     public int getScore() {
         return 0;
     }
--- src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
@@ -2,13 +2,14 @@ package com.virjanand.bowlinggame;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingGameTest {
 
     @Test
     void sumRolls() {
-        BowlingGame game = new BowlingGame();
+        BowlingGame game = new BowlingGame(asList("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"));
         assertThat(game.getScore()).isEqualTo(20);
     }
 }
