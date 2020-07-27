6dd2ef2d4e59448310ba5dfc47f26b54eb511656
Virjanand <virjanand@virjanand.com>
2020-07-27 06:24:10 +0200

    Red test: strike after strike is added as 10 points

--- src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
@@ -17,7 +17,9 @@ public class BowlingGameTest {
                 Arguments.of(asList("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"), 20, "Sum the regular rolls"),
                 Arguments.of(asList("-", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 0, "Miss(-) is 0 points"),
                 Arguments.of(asList("X", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 10, "Strike(X) is 10 points"),
-                Arguments.of(asList("X", "1", "1", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 14, "Add next two rolls to strike")
+                Arguments.of(asList("X", "1", "1", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 14, "Add next two rolls to strike"),
+                Arguments.of(asList("X", "X", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"), 33, "strike after strike is added as 10 points")
+
         );
     }
 
