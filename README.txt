e41269ccb73379d70fa141bd89b3bfef3a8f728c
Virjanand <virjanand@virjanand.com>
2020-08-07 05:09:31 +0200

    Red test: Extra roll for last spare is not treated as strike

--- README.txt
+++ README.txt
@@ -37,5 +37,5 @@ X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
 - [x] spare (roll and /) together ten points: "1", "/", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" -> 10
 ~- [ ] refactor if statements to use polymorphism~
 - [x] add next roll to spare score: "1", "/", "1", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" -> 13
-- [ ] check for strikes at end: "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", "X", "X" -> getScore -> 60
+- [x] check for strikes at end: "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", "X", "X" -> getScore -> 60
 - [ ] check for spare at end: "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "/", "X" -> getScore -> 20
--- src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/virjanand/bowlinggame/BowlingGameTest.java
@@ -14,15 +14,24 @@ public class BowlingGameTest {
 
     private static Stream<Arguments> rollsWithScores() {
         return Stream.of(
-                Arguments.of(asList("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"), 20, "Sum the regular rolls"),
-                Arguments.of(asList("-", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 0, "Miss(-) is 0 points"),
-                Arguments.of(asList("X", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 10, "Strike(X) is 10 points"),
-                Arguments.of(asList("X", "1", "1", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"), 14, "Add next two rolls to strike"),
-                Arguments.of(asList("X", "X", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"), 33, "Strike after strike is added as 10 points"),
-                Arguments.of(asList("1", "/", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"), 10, "Spare (roll and /) together ten points"),
-                Arguments.of(asList("1", "/", "1", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"), 13, "Add next roll to spare score"),
-                Arguments.of(asList("-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", "X", "X"), 30, "Extra rolls for last strike are not treated as strikes")
-//                Arguments.of(asList("-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "/", "X"), 20, "Extra roll for last spare is not treated as strike")
+                Arguments.of(asList("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"),
+                        20, "Sum the regular rolls"),
+                Arguments.of(asList("-", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"),
+                        0, "Miss(-) is 0 points"),
+                Arguments.of(asList("X", "-", "-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"),
+                        10, "Strike(X) is 10 points"),
+                Arguments.of(asList("X", "1", "1", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-","-", "-"),
+                        14, "Add next two rolls to strike"),
+                Arguments.of(asList("X", "X", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                        33, "Strike after strike is added as 10 points"),
+                Arguments.of(asList("1", "/", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                        10, "Spare (roll and /) together ten points"),
+                Arguments.of(asList("1", "/", "1", "1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                        13, "Add next roll to spare score"),
+                Arguments.of(asList("-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "X", "X", "X"),
+                        30, "Extra rolls for last strike are not treated as strikes"),
+                Arguments.of(asList("-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "/", "X"),
+                        20, "Extra roll for last spare is not treated as strike")
         );
     }
 
